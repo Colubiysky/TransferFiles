51c0359f8fd1c45e80175a37f52f0e23b2757a0e
DESKTOP-OP3M551\cola <cola_owl@pm.me>
2020-07-22 01:08:20 +0300

    minor upd

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Client
 {
     public class Client
     {
+        // адрес и порт сервера, к которому будем подключаться
+        int port = 1337; // порт сервера
+        public string Address { get => address; set => address = value; }
+        string address = String.Empty; // адрес сервера
+
+        public string FilePath { get => filePath; set => filePath = value; }
+        string filePath = String.Empty;
+        void Run()
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                Console.Write("Введите сообщение:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("ответ сервера: " + builder.ToString());
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
     }
 }
